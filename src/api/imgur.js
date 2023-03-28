86d468fe9a8515f7c540d4ac9a1c1cdaa6d736b1
Juan Pablo Gaviria <jgavirias13@gmail.com>
2023-03-28 10:26:28 -0500

    Fetching images from imgur api

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,4 +1,5 @@
 import qs from 'qs';
+import axios from 'axios';
 
 const CLIENT_ID = 'bce4691973bd288';
 const API_URL = 'https://api.imgur.com';
@@ -10,5 +11,13 @@ export default {
       response_type: 'token'
     }
     window.location = `${API_URL}/oauth2/authorize?${qs.stringify(queryString)}`;
+  },
+
+  fetchImages(token) {
+    return axios.get(`${API_URL}/3/account/me/images`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
   }
 }
